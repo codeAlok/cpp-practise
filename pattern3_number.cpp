938283b2e9de6e8f620a152fcac816f2a86767f3
alok <alok60541@gmail.com>
2022-07-28 21:10:01 +0530

    no. pattern

---
+++ pattern3_number.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<"* ";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+/*
+5
+*   
+* * 
+* * *
+* * * *
+* * * * *
+*/
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int row=1;
+//     while(row<=n){
+//         int col=1;
+//         while(col <= row){
+//             cout<< row <<" ";
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+// }
+
+/*
+5
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+
+// int main(){
+//     int n,count=0;
+//     cin>>n;
+
+//     int row=1;
+//     while(row <= n){
+//         int col=1;
+//         while(col <= row){
+//             count++;
+//             cout<< count <<" ";
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+
+// }
+
+/*
+5
+1 
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
+// int main(){
+//     int n,count=0;
+//     cin>>n;
+
+//     int row=1;
+//     while(row <= n){
+//         int col=1;
+//         int repeat = row; // solve without this
+//         while(col <= row){
+//             cout<< repeat <<" ";
+//             repeat++;
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+
+// }
+
+/*
+5
+1
+2 3
+3 4 5
+4 5 6 7
+5 6 7 8 9
+
+*/
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int i=1;
+//     while(i <= n){
+//         int j=1;
+//         while(j <= i){
+//             cout<< i-j+1<<" "; // think about this logic
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+/*
+5
+1 
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+
+*/
+
+
+
