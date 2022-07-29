28137b15feb5b0ad9b4c91d6abb54dbb017d7657
alok <alok60541@gmail.com>
2022-07-29 20:10:44 +0530

    no_of_word

---
+++ 3_count_no_word.cpp
@@ -0,0 +1,20 @@
+// **** count the number of words in a strings *****
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  string str = "HI AMY AND JAY";
+  int n = str.length();
+  int spaces=0;
+  
+  for(int i=0; i<n; i++){
+      if(str[i]==' ')
+        spaces= spaces+1;
+  }
+  
+  cout<<"The number of words are "<< spaces+1;
+
+}
