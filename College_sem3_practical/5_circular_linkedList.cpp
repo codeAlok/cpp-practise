cc7233f5e09d6f96587aa249bfe042ddb8dce552
alok <alok60541@gmail.com>
2022-08-04 22:02:36 +0530

    circular ll Quest

---
+++ College_sem3_practical/5_circular_linkedList.cpp
@@ -0,0 +1,18 @@
+//Q5. WAP for circular linkedList , with all basic functions like( insert, delete, search, count , display)
+
+#include<iostream>
+using namespace std;
+
+// **** Structure for linkedlist *****
+struct queue
+{
+    
+}
+
+
+int main()
+{
+
+
+    return 0;
+}
