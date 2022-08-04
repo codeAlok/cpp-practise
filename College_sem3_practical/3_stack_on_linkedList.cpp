db1de81e93d8475c868e3ecbdeb44058513148bd
alok <alok60541@gmail.com>
2022-08-04 22:01:50 +0530

    stack on ll Quest

---
+++ College_sem3_practical/3_stack_on_linkedList.cpp
@@ -0,0 +1,15 @@
+// Q3. WAP for Linkedlist  implementation in stack , with all basic function (append, delete , search, count, display);
+
+
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+
+
+    return 0;
+}
